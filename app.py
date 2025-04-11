f46a8496afca422558b34317d8ea0073723070fd
Antonio E. Martinez <101238873+2003antonio@users.noreply.github.com>
2025-04-11 03:59:22 +0000

    two \n that idk even know how they got there

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if not firebase_admin._apps:
         "type": firebase_config["type"],
         "project_id": firebase_config["project_id"],
         "private_key_id": firebase_config["private_key_id"],
-        "private_key": firebase_config["private_key"].replace('\n', '\n'),
+        "private_key": firebase_config["private_key"],
         "client_email": firebase_config["client_email"],
         "auth_uri": firebase_config["auth_uri"],
         "token_uri": firebase_config["token_uri"],
